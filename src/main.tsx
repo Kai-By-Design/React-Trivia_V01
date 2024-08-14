3cda96f66e61cdd49a88863d14796be96a0f83d9
Kai <pannu.kai@gmail.com>
2024-08-14 11:27:08 -0700

    First Commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+// READ ME :
+// https://youtu.be/F2JCjVSZlG0?si=7ADJEjZEoFIHneA0
+// STOPPED AT 42:49
+// THE FRAMEWORK IS IN PLACE, BUT THE INTERFAACE HAS NOT BEEN CONNECTED
+// API IS CONNECTED
